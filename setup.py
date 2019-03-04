3030be07cd3cf183adea634b066337bcd07074d6
Mateusz Lapsa-Malawski <m@cr3.io>
2019-03-04 00:09:58 +0000

    fix pypi

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 ROOT_DIR = os.path.dirname(__file__)
 SOURCE_DIR = os.path.join(ROOT_DIR)
 
-VERSION = '0.1.7'
+VERSION = '0.1.8'
 
 setup(
     name="django-dowser",
@@ -12,25 +12,22 @@ setup(
 
     author="Mateusz Lapsa-Malawski",
     author_email="m@cr3.io",
-    url="http://github.com/munhitsu/django-dowser",
 
     description="Django fork of amazing memory leaks tracking application for python wsgi - the Dowser",
     long_description=open("README.rst").read(),
+    long_description_content_type='text/x-rst',
+
+    url="http://github.com/munhitsu/django-dowser",
+
     keywords=["django", "dowser", "debug", "memory leak"],
-    license=open("LICENSE").read(),
-    download_url="http://pypi.python.org/packages/source/d/django-dowser/django-dowser-%s.tar.gz" % VERSION,
-    platforms="",
     classifiers=[
         "Programming Language :: Python",
         "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.7",
         "Intended Audience :: Developers",
-        "Natural Language :: English",
         "Operating System :: OS Independent",
-        "Environment :: Other Environment",
-        "Topic :: Utilities",
-        "Topic :: Software Development :: Libraries",
+        "Topic :: Software Development :: Debuggers",
         "License :: OSI Approved :: MIT License",
     ],
     packages=['django_dowser'],
